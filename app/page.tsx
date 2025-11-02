684b500d65e3d5c52596655bdbd92f66d28c153c
Sobhan Asadi <sobhanasadi703@gmail.com>
2025-11-02 12:28:42 +0330

    add global.d.ts & prettier.config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 /** @format */
 
 export default function Home() {
-  return (
-    <div className='border border-amber-300 h-14'>
-      <p className='text-2xl text-red-500'>start Next.js</p>
-    </div>
-  );
+  return <div></div>;
 }
